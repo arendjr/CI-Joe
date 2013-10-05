14ec570bfc93d1b6597eaf6dc2fc81948c533587
Arend van Beelen <arendjr@gmail.com>
2013-10-06 00:28:24 +0200

    Successfully spawn local child slave

--- app/slave.js
+++ app/slave.js
@@ -1,12 +1,35 @@
 "use strict";
 
 
+var _ = require("lodash");
+
+
 function main() {
 
     var yaml = require("yaml-config");
     var config = yaml.readConfig("config/app.yaml");
 
-    console.log("I'm the slave, shell=" + config.shell);
+    var argv = require("optimist")
+               .usage("Usage: $0 --name=<slave-name> [--host=<server-host> --port=<server-port>]")
+               .demand(["name"])
+               .argv;
+
+    var slaveName = argv.name;
+    var slaveConfig = _.find(config.slaves, { name: slaveName });
+    if (!slaveConfig) {
+        console.log("No such slave \"" + slaveName + "\". Exit.");
+        process.exit(1);
+    }
+
+    var masterConfig = _.clone(config.server);
+    if (argv.host && argv.port) {
+        masterConfig.hostname = argv.host;
+        masterConfig.port = argv.port;
+    }
+
+    var Slave = require("../lib/slave");
+    var slave = new Slave(slaveConfig, { masterConfig: masterConfig });
+    slave.connect();
 }
 
 main();
