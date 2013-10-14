b6daa976fbbdcee1f988255e385394dd975aaf2b
Arend van Beelen <arendjr@gmail.com>
2013-10-14 22:54:02 +0200

    Improve master-slave connections + add frontend skeleton

--- app/master.js
+++ app/master.js
@@ -9,13 +9,22 @@ function main() {
     var yaml = require("yaml-config");
     var Laces = require("laces.js");
     var config = new Laces.Model(yaml.readConfig("config/app.yaml", "active"));
+    if (config.server.scheme !== "http" && config.server.scheme !== "https") {
+        console.log("Server scheme should be 'http' or 'https'.");
+        process.exit(1);
+    }
+    if (!config.server.hostname || !config.server.port) {
+        console.log("Server hostname or port not set.");
+        process.exit(1);
+    }
+
     config.bind("change", function() {
         yaml.updateConfig(_.cloneDeep(config), "config/app.yaml", "active");
     });
 
     var express = require("express");
     var app = express();
-    var server = require("http").createServer(app);
+    var server = require(config.server.scheme).createServer(app);
     var io = require("socket.io").listen(server);
 
     app.get("/", function(req, res){
@@ -23,17 +32,6 @@ function main() {
     });
 
     app.use(express.static("www"));
-
-    io.set("log level", 1);
-    io.sockets.on("connection", function(socket) {
-        socket.on("client", function(data) {
-            console.log("client", data);
-        });
-
-        socket.on("slave", function(data) {
-            console.log("slave", data);
-        });
-    });
 
     var SlaveDriver = require("../lib/slavedriver");
     var slaveDriver = new SlaveDriver(config);
@@ -45,6 +43,24 @@ function main() {
         });
     });
 
+    io.set("log level", 1);
+    io.sockets.on("connection", function(socket) {
+        socket.on("client", function(data) {
+            console.log("client", data);
+        });
+
+        socket.on("slave", function(data) {
+            var slaveName = data.name;
+            _.each(slaveDriver.slaves, function(slave) {
+                if (slave.name === slaveName) {
+                    slave.assignSocket(socket);
+
+                    console.log("Slave '" + slaveName + "' connected.");
+                }
+            });
+        });
+    });
+
     var CommandPost = require("../lib/commandpost");
     var commandPost = new CommandPost(config, slaveDriver);
 
@@ -53,6 +69,15 @@ function main() {
     apiController.attachTo(app);
 
     server.listen(config.server.port);
+
+    function terminate() {
+        slaveDriver.destruct();
+        server.close();
+        process.exit();
+    }
+
+    process.on("SIGINT", terminate);
+    process.on("SIGTERM", terminate);
 }
 
 main();
